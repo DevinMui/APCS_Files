4d458a8a04d531834c7f74530929de8f1d4f516b
Devin Mui <devinmui@yahoo.com>
2016-08-25 17:43:39 -0700

    sdf

--- bluej/HW2/textAndNumbers/Test.java
+++ bluej/HW2/textAndNumbers/Test.java
@@ -5,7 +5,7 @@ public class Test
 {
     public static void main(String[] args)
     {
-        System.out.println(  );
-        System.out.println(  );
+        System.out.println( 3 + 4 + 5 );
+        System.out.println( "3 + 5 + 5" );
     }
 }
